e0ce6f8f0678e899da0820bcbb546706df5c37a7
AniketNevaskar <141707969+AniketNevaskar@users.noreply.github.com>
2023-08-08 12:07:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,62 @@
+import streamlit
+import pandas
+import requests
+# import snowflake.connector
+from urllib.error import URLError
 
+ 
+
+ 
+
+streamlit.title('My parents new Healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 Blueberry Oatmeal')
+streamlit.text('🥗 Kale,Spinach & Rocket Smootie')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+ 
+
+streamlit.header('🍌🥭 Build Your Fruit Own Smoothie 🥝🍇')
+
+ 
+
+#import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+ 
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+ 
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
+
+# streamlit.header('Fruityvice Fruit Advice!')
+# try:
+#   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#   if not fruit_choice:
+#     streamlit.error("please select a fruit to get information.")
+#   else:
+#       back_from_functon = get_fruityvice_data(fruit_choice)
+#       streamlit.dataframe(back_from_functon )
+
+# except URLError as e:
+#    streamlit.error()
